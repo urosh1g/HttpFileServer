078d259d45560cb6f13366249dffda989854ea9e
Urosh <urosh_h@protonmail.com>
2023-04-17 16:53:11 +0200

    Add forgotten lock for writing to cache

--- Middleware/CacheMiddleware.cs
+++ Middleware/CacheMiddleware.cs
@@ -9,10 +9,13 @@ using Helpers;
 public class CacheMiddleware : AbstractMiddleware {
 
     private LRUCache cache;
-    public CacheMiddleware(AbstractMiddleware? next = null) : base(next) {
-        cache = new LRUCache();
+    private object locker;
+    public CacheMiddleware(int capacity = 32, ColorScheme? colorScheme = null, AbstractMiddleware? next = null) : base(colorScheme, next) {
+        cache = new LRUCache(capacity);
+        locker = new object();
     }
     public override async Task HandleRequest(Context context){
+        StartColor();
         Console.WriteLine("[Caching middleware begin]");
         int key = context.httpContext.Request.RawUrl!.GetHashCode();
         var body = cache.get(key);
@@ -20,16 +23,20 @@ public class CacheMiddleware : AbstractMiddleware {
         if(body != null) {
             Console.WriteLine($"Cache hit for {context.httpContext.Request.RawUrl}");
             context.outputStream.Write(body);
-            return;
         }
         else {
             Console.WriteLine("Cache miss, calling next handler");
+            StopColor();
             if(next != null) {
                 await Task.Run(() => next.HandleRequest(context));
             }
+            StartColor();
             Console.WriteLine("Saving response to cache");
-            cache.insert(key, context.outputStream.GetBuffer());
+            lock(locker){
+                cache.insert(key, context.outputStream.GetBuffer());
+            }
         }
         Console.WriteLine("[Caching middleware end]");
+        StopColor();
     }
 }
